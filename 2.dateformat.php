56313de742982560d81637e0c83a0d51dd2a224e
Shytliuk Ihor <shytliuk.ihor@pdffiller.team>
2018-12-05 21:16:05 +0200

    l2 hw

---
+++ 2.dateformat.php
@@ -0,0 +1,10 @@
+AR: Wednesday, 22 November, 2018<br>
+
+ER: <?php
+
+//Wednesday, 22 November, 2018
+$format = "l, d F, Y";
+$date = date($format);
+echo $date;
+
+?>
---
+++ 2.expression.php
@@ -0,0 +1,10 @@
+<img src="http://dl3.joxi.net/drive/2018/12/04/0008/0809/541481/81/3c548ca284.png">
+<br>
+
+<?php
+
+$d = sqrt(pow(4,10)-pow(19,4))*2*pow(M_PI,2);
+$z = log(100, 10);
+echo $d/$z;
+
+?>
