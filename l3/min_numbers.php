31eb9bb66cc1702598c3f11c7919533215ebc9c7
Shytliuk Ihor <shytliuk.ihor@pdffiller.team>
2018-12-11 01:58:11 +0200

    l3 hw

--- l3/min_numbers.php
+++ l3/min_numbers.php
@@ -2,16 +2,24 @@
 $options = getopt("a:");
 var_dump($options);
 
+$max_amount = 100000;
+
 $amount = (int) $options["a"] ?? 0;
-$currency = [500,200,100,50,20,10,5,2,1];
 
-$result = array();
-for($i = 0; $i < count($currency); $i++) {
-	if($amount/$currency[$i] >= 1) {
-		$mult = (int) floor($amount/$currency[$i]);
-		$result[$currency[$i]] = $mult;
-		$amount = $amount - $currency[$i]* $mult;
-	}
+if($amount>$max_amount) {
+	echo "max amount is " . $max_amount;
 }
+else {
+	$currency = [500,200,100,50,20,10,5,2,1];
 
-var_export($result);
+	$result = array();
+	for($i = 0; $i < count($currency); $i++) {
+		if($amount/$currency[$i] >= 1) {
+			$mult = (int) floor($amount/$currency[$i]);
+			$result[$currency[$i]] = $mult;
+			$amount = $amount - $currency[$i]* $mult;
+		}
+	}
+
+	var_export($result);
+}
