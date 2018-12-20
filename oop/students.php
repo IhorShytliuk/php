2fe5d64592aa8fae008d1809536f4ed7dd6317f0
Shytliuk Ihor <shytliuk.ihor@pdffiller.team>
2018-12-20 21:09:27 +0200

    exercise3

--- oop/students.php
+++ oop/students.php
@@ -1,8 +1,6 @@
 <?php
 
-//Now define 5 student objects and store the objects in an array called studentList. The five students are: Mike Barnes, Jim Nickerson, Jack Indabox, Jane Miller and Mary Scott. Mike is a freshman, Jim a sophomore, Jack a junior, Jane and Mary are seniors. Their respective GPAs are: 4, 3, 2.5, 3.6 and 2.7. Make sure you assign the gender when you instantiate the objects.
-
-//Then call the showMyself method on all of them. I suggest you use a loop for making the objects and a separate loop for showing the objects.
+//Use your objects from above and let each one of the 5 students study for 60, 100, 40, 300, 1000 minutes, respectively. So the first student studies 60 minutes, the second studies 100 minutes, etc. After that call the showMyself methods on all 5 again and check whether their new gpa reflects how much they studied.
 
 class Student{
 	private $firstname;
