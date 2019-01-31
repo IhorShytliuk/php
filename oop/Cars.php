fd9a2237b4fff9d317e81ead68e5c1f74b7d986b
Ihor Shytliuk <42350393+IhorShytliuk@users.noreply.github.com>
2019-01-31 21:18:49 +0200

    Update Cars.php

--- oop/Cars.php
+++ oop/Cars.php
@@ -8,11 +8,18 @@ class Car {
 	
 	protected function getInfo() 
 	{
-		return "Type: ".$this->type;
+		return "Maker: " .$this->maker
+		.PHP_EOL."Model: " .$this->model
+		.PHP_EOL."Year: " .$this->year
+		.PHP_EOL."Vin: " .$this->vin
+		.PHP_EOL."Type: " .$this->type;
 	}
 	
-	protected function setType(string $type)
-	{
+	protected function __construct($maker, $model, $year, $vin, $type) {
+		$this->maker = $maker;
+		$this->model = $model;
+		$this->year = $year;
+		$this->vin = $vin;
 		$this->type = $type;
 	}
 }
@@ -20,44 +27,41 @@ class Car {
 class PassengerCar extends Car {
 	public $complectation;
 
-	function __construct() 
+	function __construct($maker, $model, $year, $vin, $complectation) 
 	{
-		parent::setType("PassengerCar");
+		parent::__construct($maker, $model, $year, $vin, "PassengerCar");
+		$this->complectation = $complectation;
 	}
-	
+
 	public function getInfo() 
 	{
-		return parent::getInfo() ." Complectation: " . $this->complectation;
+		return parent::getInfo() .PHP_EOL."Complectation: " . $this->complectation;
 	}
 }
 
 class Truck extends Car {
 	public $loadCapacity;
 	
-	function __construct() 
+	function __construct($maker, $model, $year, $vin, $loadCapacity) 
 	{
-		parent::setType("Truck");
+		parent::__construct($maker, $model, $year, $vin, "Truck");
+		$this->loadCapacity = $loadCapacity;
 	}
 
 	public function getInfo() 
 	{
-		return parent::getInfo() ." LoadCapacity: " . $this->loadCapacity;
+		return parent::getInfo() .PHP_EOL."LoadCapacity: " . $this->loadCapacity;
 	}
 }
 
-$car1 = new PassengerCar();
-$car2 = new Truck();
-$car3 = new PassengerCar();
-
-$car1->complectation = "full";
-
-$car2->loadCapacity = "9500";
-$car3->complectation = "min";
+$car1 = new PassengerCar("Audi","A6Allroad",2003,"WAUZZZ8DZSQAZ","Max");
+$car2 = new Truck("Volvo","XC90",2008,"WAUZZZ8DZSQAZ",2500);
+$car3 = new PassengerCar("Honda","Accord",2005,"HAUZZZ8DZSQAZ","Premium");
 
 $arr = [$car1, $car2, $car3]; 
 
 foreach($arr as $car) {
-	echo $car->getInfo(). PHP_EOL;
+	echo $car->getInfo(). PHP_EOL. PHP_EOL;
 }
 
 ?>
